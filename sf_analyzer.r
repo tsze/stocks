35b5ffdcc3eab7d4deac5401ecfedb3cc4f76392
t.szegvary@gmail.com <t.szegvary@gmail.com>
2016-02-22 19:37:13 -0800

    added specific metrics for San Francisco and Marin

---
+++ sf_analyzer.r
@@ -0,0 +1,184 @@
+library('quantmod')
+# source: https://research.stlouisfed.org/fred2/search?st=san+francisco
+
+# Unemployment Rate in San Francisco County/city, CA
+# https://research.stlouisfed.org/fred2/series/CASANF0URN
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# S&P/Case-Shiller CA-San Francisco Home Price Index
+# https://research.stlouisfed.org/fred2/series/SFXRSA
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Consumer Price Index for All Urban Consumers: All items in San Francisco-Oakland-San Jose, CA (CMSA)
+# https://research.stlouisfed.org/fred2/series/CUUSA422SA0
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Per Capita Personal Income in San Francisco-Oakland-Fremont, CA (MSA)
+# https://research.stlouisfed.org/fred2/series/SANF806PCPI
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Condo Price Index for San Francisco, California
+# https://research.stlouisfed.org/fred2/series/SFXRCNSA
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Resident Population in San Francisco County/city, CA
+# https://research.stlouisfed.org/fred2/series/CASANF0POP
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Consumer Price Index for All Urban Consumers: Rent of primary residence in San Francisco-Oakland-San Jose, CA (CMSA)
+# https://research.stlouisfed.org/fred2/series/CUURA422SEHA
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# New Private Housing Units Authorized By Building Permits for San Francisco-Oakland-Fremont, CA (MSA)
+# https://research.stlouisfed.org/fred2/series/SANF806BPPRIV
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Estimate of Median Household Income for San Francisco County/City, CA
+# https://research.stlouisfed.org/fred2/series/MHICA06075A052NCEN
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Per Capita Personal Income in San Francisco County/city, CA
+# https://research.stlouisfed.org/fred2/series/PCPI06075
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Consumer Price Index for All Urban Consumers: Food in San Francisco-Oakland-San Jose, CA (CMSA)
+# https://research.stlouisfed.org/fred2/series/CUUSA422SAF1
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# All Employees: Leisure and Hospitality: Restaurants in San Francisco-Redwood City-South San Francisco, CA (MD)
+# https://research.stlouisfed.org/fred2/series/SMU06418847072250001A
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Federal Reserve Bank Discount Rates for FRB - San Francisco District
+# https://research.stlouisfed.org/fred2/series/M13012USFRB12M156NNBR
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# All Employees: Education and Health Services in San Francisco-Oakland-Hayward, CA (MSA)
+# https://research.stlouisfed.org/fred2/series/SANF806EDUHN
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Unemployment Rate in Marin County, CA
+# https://research.stlouisfed.org/fred2/series/CAMARI5URN
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Resident Population in Marin County, CA
+# https://research.stlouisfed.org/fred2/series/CAMARI5POP
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Estimate of Median Household Income for Marin County, CA
+# https://research.stlouisfed.org/fred2/series/MHICA06041A052NCEN
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Estimated Percent of People of All Ages In Poverty for Marin County, CA
+# https://research.stlouisfed.org/fred2/series/PPAACA06041A156NCEN
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Per Capita Personal Income in Marin County, CA
+# https://research.stlouisfed.org/fred2/series/PCPI06041
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Civilian Labor Force in Marin County, CA
+# https://research.stlouisfed.org/fred2/series/CAMARI5LFN
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Bachelor's Degree or Higher (5-year estimate) in Marin County, CA
+# https://research.stlouisfed.org/fred2/series/HC01ESTVC1706041
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Personal Income in Marin County, CA
+# https://research.stlouisfed.org/fred2/series/PI06041
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# 
+# 
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# St. Louis Fed Financial Stress Index
+# https://research.stlouisfed.org/fred2/series/STLFSI
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Civilian Labor Force in San Francisco County/city, CA
+# https://research.stlouisfed.org/fred2/series/CASANF0LFN
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Consumer Price Index for All Urban Consumers: Electricity in San Francisco-Oakland-San Jose, CA (CMSA)
+# https://research.stlouisfed.org/fred2/series/CUURA422SEHF01
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# San Francisco Tech Pulse
+# https://research.stlouisfed.org/fred2/series/SFTPINDM114SFRBSF
+getSymbols('SFTPINDM114SFRBSF',src='FRED')
+chartSeries(SFTPINDM114SFRBSF, subset='last 10 years')
+
+# Bachelor's Degree or Higher (5-year estimate) in San Francisco County/city, CA
+# https://research.stlouisfed.org/fred2/series/HC01ESTVC1706075
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# Consumer Price Index for All Urban Consumers: Commodities less food and beverages in San Francisco-Oakland-San Jose, CA (CMSA)
+# https://research.stlouisfed.org/fred2/series/CUURA422SACL11
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# 
+# 
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+
+# 
+# 
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+
+# 
+# 
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+
+# 
+# 
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# 
+# 
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+# 
+# 
+getSymbols('',src='FRED')
+chartSeries(, subset='last 10 years')
+
+
+
+
