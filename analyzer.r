7c518604f4fab43968c6fccfa13052c3cfac7177
Thomas Szegvary <t.szegvary@gmail.com>
2016-02-24 13:44:00 -0800

    cleaned up due to new files for indexes.r and real_estate.r

--- analyzer.r
+++ analyzer.r
@@ -145,42 +145,6 @@ getSymbols('ECBASSETS',src='FRED')
 chartSeries(ECBASSETS, subset='last 50 years')
 
 
-# NASDAQ 100 Index
-# https://research.stlouisfed.org/fred2/series/NASDAQ100
-getSymbols('NASDAQ100',src='FRED')
-chartSeries(NASDAQ100, subset='last 50 years')
-
-# NASDAQ Composite Index
-# https://research.stlouisfed.org/fred2/series/NASDAQCOM
-getSymbols('NASDAQCOM',src='FRED')
-chartSeries(NASDAQCOM, subset='last 50 years')
-
-
-# Median Sales Price of Existing Homes
-# https://research.stlouisfed.org/fred2/series/HOSMEDUSM052N
-getSymbols('HOSMEDUSM052N',src='FRED')
-chartSeries(HOSMEDUSM052N, subset='last 50 years')
-
-# Existing Home Sales
-# https://research.stlouisfed.org/fred2/series/EXHOSLUSM495S
-getSymbols('EXHOSLUSM495S',src='FRED')
-chartSeries(EXHOSLUSM495S, subset='last 50 years')
-
-# Existing Home Sales: Months Supply
-# https://research.stlouisfed.org/fred2/series/HOSSUPUSM673N
-getSymbols('HOSSUPUSM673N',src='FRED')
-chartSeries(HOSSUPUSM673N, subset='last 50 years')
-
-# Median Sales Price of Existing Homes in West Census Region
-# https://research.stlouisfed.org/fred2/series/HOSMEDUSWTM052N
-getSymbols('HOSMEDUSWTM052N',src='FRED')
-chartSeries(HOSMEDUSWTM052N, subset='last 50 years')
-
-# Mean Sales Price of Existing Homes in West Census Region
-# https://research.stlouisfed.org/fred2/series/HOSAVGUSWTM052N
-getSymbols('HOSAVGUSWTM052N',src='FRED')
-chartSeries(HOSAVGUSWTM052N, subset='last 50 years')
-
 # For drug industry see here:
 # https://research.stlouisfed.org/fred2/search?st=drug
 
@@ -190,15 +154,6 @@ getSymbols('MEHOINUSA672N',src='FRED')
 chartSeries(MEHOINUSA672N, subset='last 50 years')
 
 
-# San Francisco Tech Pulse
-# https://research.stlouisfed.org/fred2/series/SFTPINDM114SFRBSF
-getSymbols('SFTPINDM114SFRBSF',src='FRED')
-chartSeries(SFTPINDM114SFRBSF, subset='last 50 years')
-
-# Industrial Production Index
-# https://research.stlouisfed.org/fred2/series/INDPRO
-getSymbols('INDPRO',src='FRED')
-chartSeries(INDPRO, subset='last 50 years')
 
 
 
